4d896b57e5e165f2500618fe563fc02a39b4c454
Anish Chowdhury <anish.chowdhury.official@gmail.com>
2021-03-16 08:23:36 +0530

    Final Artitecture --colorization left

--- openCode.c
+++ openCode.c
@@ -4,9 +4,9 @@ int main(){
     printf("Enter ret --> ");
     scanf("%d", &start);
     int j = start;
-    printf("|SU|MO|TU|WE|TH|FR|SA\n");
+    printf("|SU||MO||TU||WE||TH||FR||SA|\n");
     //for loop for blank spaces 
-    for (int i = 0; i < start; i++)
+    for (int i = 1; i < start; i++)
     {
         printf("|%2s|", " ");
     }
@@ -16,10 +16,10 @@ int main(){
     {
         for (; (j <= 7) && ( i <= count); j++,i++)
         {
-            printf("|%d|", i);
+            printf("|%2d|", i);
         }
         printf("\n"); 
-        j=0;
+        j=1;
     }
     
     return 0;
